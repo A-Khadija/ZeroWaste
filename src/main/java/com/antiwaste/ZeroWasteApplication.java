7b65cb25bd6231c6fbf1dfa9f0761f1a8b5359f5
A-Khadija <khadijaasehnoune@gmail.com>
2025-06-19 13:12:38 +0100

    project structure

---
+++ src/main/java/com/antiwaste/ZeroWasteApplication.java
@@ -0,0 +1,13 @@
+package com.example.ZeroWaste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZeroWasteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZeroWasteApplication.class, args);
+	}
+
+}
